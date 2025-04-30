a31489f03f4c3e6c26a61df5d688ddaefe507c07
Atul Deep Singh <999.atulsingh@gmail.com>
2025-05-01 02:07:09 +0530

    color mode app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color,setColor]=useState('')
+
+  const onClick = async () => {
+    try{
+      let [tab] = await chrome.tabs.query({ active : true});
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id!},
+        args : [color],
+        func: (color)=>{
+          document.body.style.background = color
+        }
+      })
+    } catch (e) {
+      console.log(e)
+      return e
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+      <h1>COLOR MODE</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input type="color" onChange={(e)=>setColor(e.currentTarget.value)} value={color}></input>
+        <button onClick={onClick}>
+          Click Me!
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
